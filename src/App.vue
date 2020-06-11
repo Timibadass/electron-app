3f9ace2548fca096202d4861eaa1223cec0ccb0b
timibadass <omoyenitimi@gmail.com>
2020-06-11 23:42:11 +0100

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<app-header />
+		<router-view />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	import appHeader from "@/components/Header.vue";
+	export default {
+		name: "layout",
+		components: {
+			appHeader,
+		},
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		background-color: #fff;
+	}
+
+	#nav {
+		padding: 30px;
+	}
+
+	#nav a {
+		font-weight: bold;
+		color: #2c3e50;
+	}
+
+	#nav a.router-link-exact-active {
+		color: #42b983;
+	}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+	<header class="header">
+		<h3>News App</h3>
+		<nav class="header__nav">
+			<div class="nav__link">
+				<h4>
+					<router-link to="/home">Home</router-link>
+				</h4>
+			</div>
+		</nav>
+	</header>
+</template>
+
+<script>
+	export default {
+		name: "app-header",
+	};
+</script>
+
+<style>
+	.header {
+	}
+</style>
