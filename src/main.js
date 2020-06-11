cda3813b860101bd0025ce33af6e0e7bec1e63d5
timibadass <omoyenitimi@gmail.com>
2020-06-12 00:27:12 +0100

    set up vuex for app

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import instance from '@/plugins/axios'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import instance from "@/plugins/axios";
+import router from "./router";
+import store from "./store/index";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.prototype.$axios = instance
+Vue.prototype.$axios = instance;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
