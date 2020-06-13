ccc52f56c8bda748bb1f0467f04de77ce82ca251
timibadass <omoyenitimi@gmail.com>
2020-06-14 00:00:35 +0100

    set up options for news data

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ let baseURL = `https://newsapi.org/v2`;
 let apiKey = process.env.VUE_APP_APIKEY;
 const instance = axios.create({
     baseURL: baseURL,
-    timeout: 1000,
+    timeout: 30000,
     headers: {
         "X-Api-Key": apiKey,
     },
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,52 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "../plugins/axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state: {},
-    mutations: {}
-})
+    state: {
+        countries: [{
+                name: "United States of America",
+                value: "us",
+            },
+            {
+                name: "Nigeria",
+                value: "ng",
+            },
+            {
+                name: "Argentina",
+                value: "ar",
+            },
+            {
+                name: "Canada",
+                value: "ca",
+            },
+            {
+                name: "South Africa",
+                value: "za",
+            },
+        ],
+        category: [
+            "entertainment",
+            "general",
+            "health",
+            "science",
+            "business",
+            "sports",
+            "technology",
+        ],
+    },
+    mutations: {},
+    actions: {
+        async getTopNews(context, country) {
+            let res = await axios({
+                url: `/top-headlines?country=${country}`,
+                method: "GET",
+            });
+            return res;
+        },
+    },
+});
 
-export default store
+export default store;
