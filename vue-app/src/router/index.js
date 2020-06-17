2b6d49047c01e5a907d30753939edd0e974d7c18
timibadass <omoyenitimi@gmail.com>
2020-06-17 23:30:00 +0100

    fix electron build bug.

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -20,7 +20,7 @@ const routes = [{
 ]
 
 const router = new VueRouter({
-    mode: 'history',
+    mode: process.env.IS_ELECTRON ? 'hash' : 'history',
     base: process.env.BASE_URL,
     routes
 })
