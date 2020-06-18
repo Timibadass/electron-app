504de34a3756682ad27df703a93d2bac409ec773
timibadass <omoyenitimi@gmail.com>
2020-06-18 18:28:24 +0100

    add lazy loading to component

--- vue-app/src/components/NewsCard.vue
+++ vue-app/src/components/NewsCard.vue
@@ -11,7 +11,12 @@
 			<h5 class="article__published">{{ new Date(article.publishedAt) }}</h5>
 		</div>
 		<div class="image__container">
-			<img class="news__img" :src="article.urlToImage" :alt="article.title" />
+			<img
+				class="news__img"
+				src="../assets/logo.png"
+				:data-src="article.urlToImage"
+				:alt="article.title"
+			/>
 		</div>
 	</section>
 </template>
@@ -22,6 +27,52 @@
 		props: {
 			article: Object,
 		},
+		mounted() {
+			this.lazyLoadImages();
+		},
+		methods: {
+			lazyLoadImages() {
+				const images = document.querySelectorAll(".news__img");
+
+				const options = {
+					// If the image gets within 50px in the Y axis, start the download.
+					root: null, // Page as root
+					rootMargin: "0px",
+					threshold: 0.1,
+				};
+
+				const fetchImage = (url) => {
+					return new Promise((resolve, reject) => {
+						const image = new Image();
+						image.src = url;
+						image.onload = resolve;
+						image.onerror = reject;
+					});
+				};
+
+				const loadImage = (image) => {
+					const src = image.dataset.src;
+					fetchImage(src).then(() => {
+						image.src = src;
+					});
+				};
+
+				const handleIntersection = (entries) => {
+					entries.forEach((entry) => {
+						if (entry.intersectionRatio > 0) {
+							loadImage(entry.target);
+						}
+					});
+				};
+
+				// The observer for the images on the page
+				const observer = new IntersectionObserver(handleIntersection, options);
+
+				images.forEach((img) => {
+					observer.observe(img);
+				});
+			},
+		},
 	};
 </script>
 
@@ -29,7 +80,6 @@
 	.news {
 		width: 100%;
 		display: flex;
-		/* flex-wrap: wrap; */
 		flex-direction: row;
 		align-items: flex-start;
 		max-width: 550px;
@@ -43,11 +93,6 @@
 		width: 100%;
 		max-width: 350px;
 		margin-right: 5px;
-	}
-	.image__container {
-		width: 100%;
-		max-width: 150px;
-		max-height: 150px;
 	}
 
 	.news__title {
@@ -65,8 +110,21 @@
 		text-align: left;
 	}
 
+	.image__container {
+		width: 100%;
+		max-width: 180px;
+		max-height: 180px;
+	}
+
 	.news__img {
-		max-width: 100%;
+		transition: max-width 300ms cubic-bezier(0.4, 0, 1, 1),
+			max-height 300ms cubic-bezier(0.4, 0, 1, 1);
+		max-width: 150px;
+		max-height: 150px;
+	}
+
+	.news__img:hover {
+		max-width: 180px;
 		max-height: 180px;
 	}
 
