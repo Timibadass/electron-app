1a03f6ce5a3cb0a224e640d400e5db8c68917054
timibadass <omoyenitimi@gmail.com>
2020-06-20 21:44:40 +0100

    refactor random country index generator into getters

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -3,47 +3,43 @@
 		<h1>Welcome to News App</h1>
 		<h4>Displaying Top News from {{ countryInfo.name }}</h4>
 		<div class="articles__div" v-if="articles">
-			<news-card
-				v-for="(article, index) in articles"
-				:key="index"
-				:article="article"
-			></news-card>
+			<news-card v-for="(article, index) in articles" :key="index" :article="article"></news-card>
 		</div>
 	</section>
 </template>
 
 <script>
-	import { mapActions, mapState } from "vuex";
+	import { mapActions, mapState, mapGetters } from "vuex";
 	import NewsCard from "../components/NewsCard";
 
 	export default {
 		data() {
 			return {
 				articles: "",
-				countryInfo: "",
+				countryInfo: ""
 			};
 		},
 		components: {
-			NewsCard,
+			NewsCard
 		},
 		mounted() {
 			this.fetchTopNews();
 		},
 		computed: {
 			...mapState(["countries"]),
+			...mapGetters(["getRandomCounty"])
 		},
 		methods: {
 			...mapActions(["getTopNews"]),
 			async fetchTopNews() {
-				let countriesLength = this.countries.length;
-				let countryIndex = Math.floor(Math.random() * (countriesLength - 1) + 1);
+				let countryIndex = this.getRandomCounty;
 
 				this.countryInfo = this.countries[countryIndex];
 
 				let { data } = await this.getTopNews(this.countries[countryIndex].value);
 				this.articles = data.articles;
-			},
-		},
+			}
+		}
 	};
 </script>
 
